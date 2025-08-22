51cb590c81b773aad6c230327e4ff56d5fc87899
xiaoping wu <wuxxx184@umn.edu>
2025-08-22 09:15:46 -0500

    Update girf_calib.m

--- pulseq_skope/girf_calib.m
+++ pulseq_skope/girf_calib.m
@@ -2,15 +2,14 @@
 % Author: Sebastian Rosenzweig <sebastian.rosenzweig@skope.ch>
 % (c) 2021 Skope Magnetic Resonance Technologies AG
 
-close all; clc; clear all;
-addpath(genpath('../pulseq/')); % Pulseq path
-addpath('~/matlab/skope_TCP_client/methods')
-addpath('~/matlab/skope_MatlabAqSysDataImport/bin')
+clearvars;
+addpath(genpath(fullfile('..','pulseq')));
+%addpath('~/matlab/skope_TCP_client/methods')
 
 %% Scanner specs
 seq_name = "GIRF_calib";          
 seq_type = "GIRF Calibration";
-scanner_type = "Siemens dotplus 10.5T HG LP"; % "Siemens dotplus 10.5T SC72CD"; Siemens Terra 7T SC72CD";
+scanner_type = "Siemens Terra 7T SC72CD"; % "Siemens dotplus 10.5T HG LP"; % "Siemens dotplus 10.5T SC72CD"; Siemens Terra 7T SC72CD";
 
 sk = Skope(seq_name, seq_type, scanner_type);
 seq_params = struct('trigChannel', 'osc0'); % 'osc0','osc1','ext1'
@@ -20,7 +19,7 @@ seq_params.useArbitraryGrad= false; % false to use mr.makeTrapezoid and true to 
 
 seq_params.gradSafetyMargin= 0.9;
 %%% tcp control
-host = '134.84.19.164'; %'skope-105T'. if empty, no measurement automation for field monitoring.
+% host = '134.84.19.164'; %'skope-105T'. if empty, no measurement automation for field monitoring.
 %host_moco = '134.84.19.37'; %'skope-terra'. if empty, no measurement automation for motion detection.
 
 %%
@@ -37,10 +36,10 @@ sk.Write('external');
 sk.Check_timing();
 sk.Plot(36);
 
-%% tcp control
-% field monitoring
-if exist('host','var')&& ~isempty(host)
-    tcpAutomateScan([fn,'.seq'], host, false);
-end
+% %% tcp control
+% % field monitoring
+% if exist('host','var')&& ~isempty(host)
+%     tcpAutomateScan([fn,'.seq'], host, false);
+% end
 
 
